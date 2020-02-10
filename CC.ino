3347ab32d0e040e8685827f9ee2b8f848bec058a
SPChip <taskomport@mail.ru>
2020-02-10 22:53:32 +0300

    edit cc

--- CC.ino
+++ CC.ino
@@ -4,14 +4,20 @@ void CruiseControl(){   // функция круиз контроля
   if (currentSpeed < presetSpeed){   // если текущая скорость меньше установленной
    REGULATOR.setDirection(NORMAL);   // включаем прямое регулирование ПИДа
    DAC_BRAKE.setVoltage(MIN_REGULATOR_OUT*10, false);   // устанавливаем выход ЦАП тормоза в 0
-   DAC_ACCEL.setVoltage(REGULATOR.getResultTimer()*10, false);   // регулируем скорость ЦАПом газа
-   Serial.println(REGULATOR.getResultTimer());
+   DAC_ACCEL.setVoltage(map (REGULATOR.getResultTimer(),0 , 500, 800, 3700), false);   // регулируем скорость ЦАПом газа
+   
   }
-  if (currentSpeed > presetSpeed){   // если текущая скорость больше установленной
-   //REGULATOR.setDirection(REVERSE);  // включаем обоатное регулирование ПИДа
+ // if (currentSpeed > presetSpeed){   // если текущая скорость больше установленной
+ //  REGULATOR.setDirection(REVERSE);  // включаем обоатное регулирование ПИДа
    DAC_ACCEL.setVoltage(MIN_REGULATOR_OUT*10, false);   // устанавливаем выход ЦАП газа в 0
-   //DAC_BRAKE.setVoltage(REGULATOR.getResultTimer()*10, false);   // регулируем скорость ЦАПом тормоза
-  }
-  
-  //Serial.println(presetSpeed);
+//  DAC_BRAKE.setVoltage(map (REGULATOR.getResultTimer(),0 , 500, 800, 3700), false);   // регулируем скорость ЦАПом тормоза
+//  }
+  //Serial.println(REGULATOR.getResultTimer());
+   if (TEST_TIMER.isReady()) {
+  Serial.print(presetSpeed);
+  Serial.print(",");
+  Serial.print(currentSpeed);
+Serial.println(",");
+
+   }
 }
