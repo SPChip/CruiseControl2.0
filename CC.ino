b73e73709c6984622b6a3f632c0a023be478a225
SPChip <taskomport@mail.ru>
2020-02-11 16:49:40 +0300

    edit cc

--- CC.ino
+++ CC.ino
@@ -1,23 +1,27 @@
-void CruiseControl(){   // функция круиз контроля
-  REGULATOR.setpoint = presetSpeed;  // отправляем в регулятор установленную скорость
-  REGULATOR.input = currentSpeed;    // отправляем в регулятор текущую скорость
-  if (currentSpeed < presetSpeed){   // если текущая скорость меньше установленной
-   REGULATOR.setDirection(NORMAL);   // включаем прямое регулирование ПИДа
-   DAC_BRAKE.setVoltage(MIN_REGULATOR_OUT*10, false);   // устанавливаем выход ЦАП тормоза в 0
-   DAC_ACCEL.setVoltage(map (REGULATOR.getResultTimer(),0 , 500, 800, 3700), false);   // регулируем скорость ЦАПом газа
-   
+void CruiseControl() {                       // функция круиз контроля
+  static long timeCalc;                      // для вычисления времени между появлением новых данных о скорости
+  REGULATOR.setpoint = presetSpeed;          // отправляем в регулятор установленную скорость
+  if (newSpeedFlag) {
+    REGULATOR.setDt(millis()-timeCalc);      // устанавилваем dt ПИДа равным времени обновления данных о скорости
+    timeCalc = millis();                     // засекаем время
+    newSpeedFlag = 0;                        // опускаем флаг новых данных о скорости    
+    REGULATOR.input = currentSpeed;          // отправляем в регулятор текущую скорость
+    if (currentSpeed < presetSpeed) {        // если текущая скорость меньше установленной
+      REGULATOR.setDirection(NORMAL);        // включаем прямое регулирование ПИДа
+      DAC_BRAKE.setVoltage(MIN_REGULATOR_OUT * 10, false);        // устанавливаем выход ЦАП тормоза в 0
+      DAC_ACCEL.setVoltage(map (REGULATOR.getResult(), 0 , 500, 800, 3700), false);          // регулируем скорость ЦАПом газа
+    }
+     if (currentSpeed > presetSpeed){          // если текущая скорость больше установленной
+    //  REGULATOR.setDirection(REVERSE);          // включаем обоатное регулирование ПИДа
+    DAC_ACCEL.setVoltage(MIN_REGULATOR_OUT * 10, false);          // устанавливаем выход ЦАП газа в 0
+    //  DAC_BRAKE.setVoltage(map (REGULATOR.getResult(),0 , 500, 800, 3700), false);           // регулируем скорость ЦАПом тормоза
+     }
   }
- // if (currentSpeed > presetSpeed){   // если текущая скорость больше установленной
- //  REGULATOR.setDirection(REVERSE);  // включаем обоатное регулирование ПИДа
-   DAC_ACCEL.setVoltage(MIN_REGULATOR_OUT*10, false);   // устанавливаем выход ЦАП газа в 0
-//  DAC_BRAKE.setVoltage(map (REGULATOR.getResultTimer(),0 , 500, 800, 3700), false);   // регулируем скорость ЦАПом тормоза
-//  }
-  //Serial.println(REGULATOR.getResultTimer());
-   if (TEST_TIMER.isReady()) {
-  Serial.print(presetSpeed);
-  Serial.print(",");
-  Serial.print(currentSpeed);
-Serial.println(",");
+  if (TEST_TIMER.isReady()) {
+    Serial.print(presetSpeed);
+    Serial.print(",");
+    Serial.print(currentSpeed);
+    Serial.println(",");
 
-   }
+  }
 }
